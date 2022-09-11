58af369a74294d1e3f9d8b0ea52ab9029e5b3212
LoganScho <schoyer.logan@outlook.be>
2022-09-11 20:03:35 +0200

    Add logger

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const { Client } = require('discord.js');
-const dotenv = require('dotenv');
-dotenv.config();
-
+const dotenv = require('dotenv'); dotenv.config();
 const client = new Client({ intents: 3276799 });
+const Logger = require('./utils/Logger');
 
 client.once('ready', () => {
   console.log("READY");
 });
+
+// Récupérer les erreurs pour éviter l'arrêt du bot à chaque fois
+process.on('exit', code => { Logger.client(`Le processus s'est arrêté avec le code: ${code} !`) });
+
+process.on('uncaughtException', (err, origin) => { 
+  Logger.error(`UNCAUGHT EXCEPTION: ${err}`);
+  console.error(`Origine: ${origin}`);
+});
+
+process.on('unhandledRejection', (reason, promise) => { 
+  Logger.warn(`UNHANDLED REJECTION: ${reason}`);
+  console.log(promise);
+});
+
+process.on('warning', (...args) => Logger.warn(...args));
 
 client.login(process.env.DISCORD_TOKEN);
